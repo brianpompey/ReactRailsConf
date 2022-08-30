27f4ce2ceb6693c3703d085d68bed8c6182de97f
Brian Pompey <brianpompey@brians-mbp.mynetworksettings.com>
2022-08-30 16:03:07 -0400

    decisions to make for donations

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::DonationsController < ApplicationController
   def create
+    # @
+  end
+
+  private
+
+  def donations_params
+    params.require(:donation).permit(:user_id, :conference_id, :amount)
   end
 end
