559d041767afc326ed1bff0451b05f0b6eebae7a
Brian Pompey <brianpompey@brians-mbp.mynetworksettings.com>
2022-08-30 16:00:46 -0400

    filled out conference controller

--- app/controllers/api/v1/conferences_controller.rb
+++ app/controllers/api/v1/conferences_controller.rb
@@ -1,10 +1,27 @@
 class Api::V1::ConferencesController < ApplicationController
   def create
+    @conference = Conference.new(conference_params)
+
+    if @conference.save
+      render json: @conference
+    else
+      render json: @conference.errors
+    end
   end
 
   def index
+    @conferences = Conference.all
+
+    render json: @conferences
   end
 
   def show
+    render json: @conference
+  end
+
+  private
+
+  def conference_params
+    params.require(:conference).permit(:name, :date, :time, :min_donation, :keynote, :category, workshops_attributes: [:name, :speaker, :description], talks_attributes: [:name, :speaker, :description])
   end
 end
