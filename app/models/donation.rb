8ef35cb5f5cd5f94736932e1c99fa7f5c4742a8e
Brian Pompey <brianpompey@brians-mbp.mynetworksettings.com>
2022-09-01 10:05:05 -0400

    Aded valid donation method

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,13 @@ class Donation < ApplicationRecord
     belongs_to :conference
 
     validates :amount, presence: true
+
+    def valid_donation(conference)
+        if self.amount < conference.min_donation
+            self.errors.add(:amount, "Apologies, but the amount you've entered is less than the required amount for this conference")
+            false
+        else
+            self.save
+        end
+    end
 end
