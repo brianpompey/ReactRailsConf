853ceec1ec1e368fe9872bd1260e359a11b3d65a
Brian Pompey <brianpompey@brians-mbp.mynetworksettings.com>
2022-09-01 10:07:40 -0400

    filled out donation controller for now

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::DonationsController < ApplicationController
   def create
-    # @
+    @conference = Conference.find(params[:donation][:conference_id])
+    @donation = Donation.new(donation_params)
+
+        
+        
+    if @donation.valid_donation(@conference)
+      flash[:notice] = "You're Registered!"
+      render json: @donation
+    else
+      render json: @donation.errors
+    end
   end
 
   private
